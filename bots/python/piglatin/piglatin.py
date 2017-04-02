2a66e863cd0ded988476fa4bbbf56fb6e76c0f72
Vasily Sovetov <vsovetov@croc.ru>
2017-04-02 23:40:04 +0300

    Add python piglatin bot

---
+++ bots/python/piglatin/piglatin.py
@@ -0,0 +1,12 @@
+import telepot
+
+with open("../../ttoken", "r") as f:
+    token = f.readline()
+
+
+def piglatinize(word):
+    return word[1:] + word[0] = "ay"
+
+
+def piglatin_handler(msg):
+    return
